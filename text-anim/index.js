87dab471fd9247bbdfbd1401442674dd2aa579ba
LaWass1864 <amwass4@hotmail.com>
2023-06-28 21:21:19 +0200

    text anim ok

--- text-anim/index.js
+++ text-anim/index.js
@@ -1,15 +1,26 @@
+
+// Pointer le id target
 const target = document.getElementById("target");
+
+// creation d'index
 let wordIndex = 0;
 let letterIndex = 0;
 
+// Données dans un tableau
 let array = ["développeuse", "photographe", "créative"];
 
+
+// creation de la fonction qui va écrire des mots
 const createLetter = () => {
+    // creation d'une balise et injecter les lettres dans des span
   const letter = document.createElement("span");
+  // appenChild est un enfant 
   target.appendChild(letter);
 
+  // On met letter dans le span
   letter.textContent = array[wordIndex][letterIndex];
 
+  // Compte a rebour pour que le mot disparaisse au bout de 2 secondes et 800 millieme
   setTimeout(() => {
     letter.remove();
   }, 2800)
