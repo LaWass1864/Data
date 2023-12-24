6694cd86420c24a114d7cc3ea6d14ebdfc0b8ee4
LaWass1864 <amwass4@hotmail.com>
2023-12-24 22:49:15 +0100

    commentaire ok

--- color-generator/index.js
+++ color-generator/index.js
@@ -1,5 +1,5 @@
 function getColor() {
-  // Créer 3 variables pour stocker 3 chiffres aléatoires
+  // Créer 3 variables pour stocker 3 chiffres aléatoires correspondant au red,green et blue
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
@@ -10,5 +10,5 @@ function getColor() {
   // Montrer sur le body la couleur rgb()
   document.body.innerHTML = `<h2>${color}</h2>`;
 }
-
+// On joue la fonction
 setInterval(getColor, 3000);
