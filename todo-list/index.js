2e59079dea8c95b2a96ca7c4435ea33d44cb9456
LaWass1864 <amwass4@hotmail.com>
2023-12-25 12:47:57 +0100

    todolist OK

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,7 +1,23 @@
 // Ajouter sur le dom ce qui est tapé dans le form à la validation du form
 const form = document.querySelector("form");
 
-// Ajouter un element en faisant Entré, c'est l'evenement sur le fom, donc on lui passe le submit ensuite on met le e.preventDefault pour que la page ne se charge pas.
+// On va créer une fonction pour stocker Storage part
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo ppur le supprimer</li>`;
+  }
+}
+
+// Jouer la fonction lors du chargement de la page
+window.addEventListener('load', getTodos)
+
+// Ajouter un element en tapant sur Entré, c'est l'evenement sur le fom, donc on lui passe le submit ensuite on met le e.preventDefault pour que la page ne se charge pas.
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,6 +25,7 @@ form.addEventListener("submit", (e) => {
   // Ces taches sont appellés item
   list.innerHTML += `<li> ${item.value}</li>`;
   item.value = "";
+  storeList();
 });
 
 // Supprimer un item : quand je vais cliquer sur un UL (meme si on ne le voit pas)
@@ -19,4 +36,5 @@ list.addEventListener("click", (e) => {
   } else {
     e.target.classList.add("checked");
   }
+  storeList();
 });
